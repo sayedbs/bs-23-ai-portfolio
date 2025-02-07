c1a54ea160af950c7f9fb2494536c8c01e797749
sayedbs <sayeed@brainstation-23.com>
2025-02-07 12:52:24 +0600

    Dashboard main added

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -2,6 +2,8 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { TabsDemo } from "./Menu";
+import Image from "next/image";
 
 export default function Dashboard() {
     const [message, setMessage] = useState("");
@@ -34,10 +36,18 @@ export default function Dashboard() {
     };
 
     return (
-        <div>
-            <h1>Dashboard</h1>
-            <p>{message}</p>
-            <button onClick={logout}>Logout</button>
+        <div className="p-3 min-h-[100vh]" style={{ backgroundImage: "url('/images/ai-1.jpg')", backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "no-repeat" }}>
+            <Image className="absolute left-5 top-5" src={"/images/bs23.png"} alt="ai" width="100" height="100" />
+
+            <button onClick={logout} className="absolute top-0 right-0 m-3 p-2 bg-white bg-opacity-50 text-white rounded-lg flex align-center gap-2 hover:bg-opacity-100 hover:text-red-500">
+                <Image src={"/images/shotdown.webp"} alt="logout" width="20" height="20" />
+                Logout
+            </button>
+
+            <div className="max-w-[1500px] mx-auto">
+                <TabsDemo />
+            </div>
+           
         </div>
     );
 }
