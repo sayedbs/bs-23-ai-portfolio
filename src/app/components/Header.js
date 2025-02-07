02fe1b2b34ef20d71fc11f392ffaf6ebc20259b4
musaberul-bs23 <ahmedmusaberul.khan@abbvie.com>
2025-02-07 19:39:41 +0600

    Header Button Design

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -33,10 +33,15 @@ function Header() {
 
             <div className="flex-1 flex items-center gap-4 justify-center">
                 <button
-                    className={`relative px-4 py-2 rounded-full ${
-                        isActive("/computer-vision") ? "bg-blue-500" : ""
-                    }`}
-                    style={{ transformStyle: "preserve-3d" }}
+                    className={`relative px-4 py-2 rounded-full transition duration-300 ${
+                        isActive("/computer-vision")
+                            ? "bg-blue-500 bg-opacity-50"
+                            : "bg-opacity-20"
+                    } hover:bg-white hover:bg-opacity-30 active:bg-opacity-50`}
+                    style={{
+                        transformStyle: "preserve-3d",
+                        backdropFilter: "blur(10px)",
+                    }}
                     onClick={() => router.push("/computer-vision")}
                 >
                     <span className="relative block text-white dark:text-white">
@@ -44,12 +49,15 @@ function Header() {
                     </span>
                 </button>
                 <button
-                    className={`relative px-4 py-2 rounded-full ${
+                    className={`relative px-4 py-2 rounded-full transition duration-300 ${
                         isActive("/artificial-intelligence")
-                            ? "bg-blue-500"
-                            : ""
-                    }`}
-                    style={{ transformStyle: "preserve-3d" }}
+                            ? "bg-blue-500 bg-opacity-50"
+                            : "bg-opacity-20"
+                    } hover:bg-white hover:bg-opacity-30 active:bg-opacity-50`}
+                    style={{
+                        transformStyle: "preserve-3d",
+                        backdropFilter: "blur(10px)",
+                    }}
                     onClick={() => router.push("/artificial-intelligence")}
                 >
                     <span className="relative block text-white dark:text-white">
@@ -57,10 +65,15 @@ function Header() {
                     </span>
                 </button>
                 <button
-                    className={`relative px-4 py-2 rounded-full ${
-                        isActive("/chatbot") ? "bg-blue-500" : ""
-                    }`}
-                    style={{ transformStyle: "preserve-3d" }}
+                    className={`relative px-4 py-2 rounded-full transition duration-300 ${
+                        isActive("/chatbot")
+                            ? "bg-blue-500 bg-opacity-50"
+                            : "bg-opacity-20"
+                    } hover:bg-white hover:bg-opacity-30 active:bg-opacity-50`}
+                    style={{
+                        transformStyle: "preserve-3d",
+                        backdropFilter: "blur(10px)",
+                    }}
                     onClick={() => router.push("/chatbot")}
                 >
                     <span className="relative block text-white dark:text-white">
@@ -74,9 +87,22 @@ function Header() {
                     onClick={logout}
                     className="text-white flex align-center justify-center gap-2 hover:bg-opacity-100 hover:text-white border-2  border-transparent hover:border-gray-600 hover:hover:bg-primary/90 py-2 px-4 rounded-full"
                 >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-right my-auto" viewBox="0 0 16 16">
-                          <path fillRule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>
-                          <path fillRule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-box-arrow-right my-auto"
+                        viewBox="0 0 16 16"
+                    >
+                        <path
+                            fillRule="evenodd"
+                            d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"
+                        />
+                        <path
+                            fillRule="evenodd"
+                            d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"
+                        />
                     </svg>
                     Logout
                 </button>
