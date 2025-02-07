f6ff87c9fd46f0d7e553d695b936b9ad8f53eb24
musaberul-bs23 <ahmedmusaberul.khan@abbvie.com>
2025-02-07 15:03:04 +0600

    Login Issue FIxed

--- src/app/components/ProtectedRoute.js
+++ src/app/components/ProtectedRoute.js
@@ -1,19 +1,26 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
 
 export default function ProtectedRoute({ children }) {
+    const [isAuthChecked, setIsAuthChecked] = useState(false);
     const pathname = usePathname();
     const router = useRouter();
     const isAuthPage = pathname === "/login";
 
     useEffect(() => {
         const auth = localStorage.getItem("auth");
+
         if (!auth && !isAuthPage) {
-            router.push("/login");
+            router.replace("/login");
+        } else {
+            setIsAuthChecked(true);
         }
     }, [pathname]);
+
+    // Prevent rendering protected content until auth check is complete
+    if (!isAuthChecked && !isAuthPage) return null;
 
     return children;
 }
