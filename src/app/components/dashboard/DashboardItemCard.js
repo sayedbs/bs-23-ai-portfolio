f04efb056a2eecc41a07cc9b1e6afc9bc3529a28
sayedbs <sayeed@brainstation-23.com>
2025-02-07 19:20:22 +0600

    Card updated

--- src/app/components/dashboard/DashboardItemCard.js
+++ src/app/components/dashboard/DashboardItemCard.js
@@ -16,8 +16,9 @@ function DashboardItemCard() {
             initial={{ opacity: 0, scale: 0.9 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.5 }}
+            
         >
-                        <Card className="transform transition-transform hover:scale-105 hover:shadow-lg bg-black bg-opacity-30 border-2 border-gray-800 hover:bg-white hover:text-gray-900 text-gray-300 hover:shadow-indigo-500/50 rounded-lg overflow-hidden backdrop-blur-3xl group transition-colors duration-300">
+            <Card className="transform hover:shadow-2xl bg-black bg-opacity-30 border-2 border-gray-800 hover:bg-white hover:text-gray-900 text-gray-300 hover:shadow-indigo-500/50 rounded-lg overflow-hidden backdrop-blur-3xl group transition-colors duration-300 hover:border-gray-200">
                 <CardHeader className="p-3 bg-gray-900 h-[200px] flex items-center justify-center">
                     <Image
                         src="/images/upe-nd-now.png"
@@ -27,16 +28,16 @@ function DashboardItemCard() {
                         className="rounded-lg"
                     />
                 </CardHeader>
-                <CardContent className="px-6 py-0 pt-5 text-sm">
+                <CardContent className="px-6 py-0 pt-5 text-sm ">
                     <h2 className="text-3xl font-bold mb-3 ">
                         UPENDNOW
                     </h2>
             
-                    <p>Upendnow is a powerful platform designed as a co-pilot for content creators. It automates the entire video production process, allowing creators...
+                    <p className="group-hover:translate-x-2 transition-transform duration-300 ">Upendnow is a powerful platform designed as a co-pilot for content creators. It automates the entire video production process, allowing creators...
                     </p>
                 </CardContent>
                 <CardFooter className="p-6 flex justify-between items-center">
-                    <Button className="bg-indigo-500 text-white max-w-[180px] ml-auto hover:bg-gray-800 p-4 rounded-full flex items-center justify-center transition-transform duration-300">
+                    <Button className="bg-indigo-500 text-white max-w-[180px] ml-auto hover:bg-gray-800 group-hover:bg-gray-800 p-4 rounded-full flex items-center justify-center transition-transform duration-300">
                         <span className="hover:translate-x-1 flex items-center transition-transform duration-300">
                             View Details 
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor"> 
