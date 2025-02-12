3cdd491a51186f498b9ac30a496c190ccbbb5a17
sayedbs <sayeed@brainstation-23.com>
2025-02-12 13:11:19 +0600

    Fav icon added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,18 @@
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 import "./globals.css";
-import Head from "next/head";
 
 export const metadata = {
     title: "BS23 - AI Portfolio",
-    description: "Showcasing cutting-edge AI projects",
+    description: "Showcasing cutting-edge AI projects"
 };
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <Head>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+            <head>
+                <link rel="icon" href="/android-chrome-512x512.png" sizes="any" />
+            </head>
             <body>
                 <AuthProvider>
                     <ProtectedRoute>{children}</ProtectedRoute>
